6af21f09713db27b8d648fad54be04fe71dcc422
Kevin Hill <hillka28@uww.edu>
2021-04-18 22:38:45 -0500

    Add mobile styling for Upcoming Events and Commitment blocks

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+new Splide( '#events-carousel', {
+	perPage    : 3,
+	perMove    : 1,
+    autoWidth  : true,
+	autoHeight : true,
+    arrows     : false,
+	focus      : 'center',
+	trimSpace  : false,
+	breakpoints: {
+		600: {
+			
+		}
+	}
+} ).mount( {  } );
